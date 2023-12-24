0adfc8b12a9fa1a13d59da639832f32cb4363cdb
Enrico Blanco <enrico.cidadeb@gmail.com>
2023-12-23 22:43:43 -0300

    ajustes front navbar footer

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -2,44 +2,41 @@
 
 import { useSession, signOut } from 'next-auth/react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 
 const NavBar = () => {
   const { data: session, status } = useSession()
 
-  const router = useRouter()
   return (
-    <div>
-      <ul className="flex flex-row justify-evenly">
-        {!session ? (
-          <>
-            <li>
-              <Link href={'/register'}>Register</Link>
-            </li>
-            <li>
-              <Link href={'/login'}>Login</Link>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>{session.user?.name}</li>
-            <li>{status}</li>
-            <li>
-              <Link href={'/register'}>{session.user?.email}</Link>
-            </li>
-            <li>
-              <button
-                onClick={() => {
-                  signOut()
-                  router.push('/')
-                }}
-              >
-                Logout
-              </button>
-            </li>
-          </>
-        )}
-      </ul>
+    <div className="py-4 text-xl bg-gray-100">
+      {!session ? (
+        <ul className="flex flex-row justify-between mx-9">
+          <li className="hover:text-black">
+            <Link href={'/register'}>Register</Link>
+          </li>
+          <li className="hover:text-black">
+            <Link href={'/login'}>Login</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="flex flex-row justify-evenly">
+          <li>{session.user?.name}</li>
+          <li>{status}</li>
+          <li>
+            <Link href={'/register'}>{session.user?.email}</Link>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                signOut().then(() => {
+                  window.location.replace('/')
+                })
+              }}
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from 'next/image'
+import Container from './components/Container'
 
 export default function Home() {
-  return <div>Restaurant</div>
+  return (
+    <Container>
+      <div className="flex justify-center min-h-fit">
+        <div className="text-5xl mt-28">Restaurants</div>
+      </div>
+    </Container>
+  )
 }
